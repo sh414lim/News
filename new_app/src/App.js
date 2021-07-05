00cabfe713b2184037940d2713b62cba92778889
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-07-05 13:19:30 +0900

    수정

--- new_app/src/App.js
+++ new_app/src/App.js
@@ -1,6 +1,7 @@
 import React,{ useCallback, useState } from 'react';
 import Categories from './Components/Categories';
 import NewsList from './Components/Newslist';
+import { Button } from 'reactstrap';
 
 
   const App=()=>{
@@ -12,6 +13,9 @@ import NewsList from './Components/Newslist';
       <>
         <Categories category={category} onSelect={onSelect}/>
         <NewsList category={category}/>
+        <Button color="info">info</Button>
+        <Button color="danger">danger</Button>
+        <Button color="success">success</Button>
 
       </>
     );
